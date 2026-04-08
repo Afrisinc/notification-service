ab5c02dac6ca1e3fa4c4524e57fe180a5332ac1f
gabbrenn <gabvladimirbrenn@gmail.com>
2026-04-08 22:50:24 +0200

    add variables in env

--- src/services/api/src/config/env.ts
+++ src/services/api/src/config/env.ts
@@ -32,6 +32,8 @@ export const env = cleanEnv(process.env, {
   // System App ID (for organization invites and system notifications)
   SYSTEM_APP_ID: str({ default: 'system-org-app' }),
   RESET_PASSWORD_TEMPLATE_ID: str({ default: 'system-reset-password' }),
+  VERIFY_EMAIL_TEMPLATE_ID: str({ default: 'system-verify-email' }),
+  WELCOME_EMAIL_TEMPLATE_ID: str({ default: 'system-welcome-email' }),
 
   // SMTP Configuration (optional)
   SMTP_HOST: str({ default: 'localhost' }),
